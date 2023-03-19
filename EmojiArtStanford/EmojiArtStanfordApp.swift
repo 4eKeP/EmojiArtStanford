f002a01786f5f5ab1d0bd3d040a4c07408e5dbce
Egor <egor_chek@mail.ru>
2023-03-19 16:47:25 +0400

    Initial Commit

---
+++ EmojiArtStanford/EmojiArtStanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtStanfordApp.swift
+//  EmojiArtStanford
+//
+//  Created by admin on 19.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtStanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
