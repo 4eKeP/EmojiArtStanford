d8057993e8bb57a7ee6094bac360f11a57f5c7d9
Egor <egor_chek@mail.ru>
2023-03-20 21:17:27 +0400

    добавлена возможность drag and drop

--- EmojiArtStanford/EmojiArtStanfordApp.swift
+++ EmojiArtStanford/EmojiArtStanfordApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct EmojiArtStanfordApp: App {
+    let document =  EmojiArtDocument()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmojiArtDocumentView(document: document)
         }
     }
 }
